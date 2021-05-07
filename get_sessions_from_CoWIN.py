68aee62cfe65410426d47ddc24111e7980bf7ded
Neelakantan K.K <neelakantankk@gmail.com>
2021-05-08 00:29:00 +0530

    Scrapes by week for a district

--- get_sessions_from_CoWIN.py
+++ get_sessions_from_CoWIN.py
@@ -4,12 +4,17 @@ import json
 import logging
 import logging.config
 import sys
+import datetime
+from calendar import monthrange
 
 ROOT_URL = r'https://cdn-api.co-vin.in/api/'
 DISTRICT_ENDPOINT = r'v2/appointment/sessions/public/findByDistrict'
+DISTRICT_CALENDAR_ENDPOINT = r'v2/appointment/sessions/public/calendarByDistrict'
 LIST_DISTRICTS_ENDPOINT = r'v2/admin/location/districts/'
 LIST_STATES_ENDPOINT = r'v2/admin/location/states'
-HEADERS = {'Accept-Language':'en_US'}
+USER_AGENT_STRING = ['Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36','Mozilla/5.0 (X11; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0']
+HEADERS = {'Accept-Language':'en_US',
+           'User-Agent':USER_AGENT_STRING[0]}
 
 def get_state_id(state_name):
     logger = logging.getLogger(__name__)
@@ -22,6 +27,8 @@ def get_state_id(state_name):
         req = requests.get(call_url,headers=HEADERS)
         if req.status_code != 200:
             logger.error(f"{call_url} returned {req.status_code}")
+            if req.status_code == 403:
+                logger.error(f"403 FORBIDDEN")
             sys.exit(1)
         else:
             states_list = req.json()
@@ -52,12 +59,14 @@ def get_districts(state_id):
         req = requests.get(call_url,headers=HEADERS)
         if req.status_code != 200:
             logger.error(f"{call_url} returned {req.status_code}")
+            if req.status_code == 403:
+                logger.error(f"403 FORBIDDEN")
             sys.exit(1)
         else:
             district_list = req.json()
             with open(f"district_list_{state_id}.json",'w') as district_list_file:
                 json.dump(district_list, district_list_file)
-    districts = map(lambda x: x['district_id'],district_list['districts'])
+    districts = map(lambda x: (x['district_id'],x['district_name']),district_list['districts'])
     logger.info(f"Returning district list")
     return districts
 
@@ -68,17 +77,50 @@ def create_date_for_query(date,month,year):
         month = f"0{month}"
     return f"{date}-{month}-{year}"
 
-def get_for_district(dist_query,date_query):
-    call_url = urllib.parse.urljoin(ROOT_URL,DISTRICT_ENDPOINT)
+def get_for_district(call_url,dist_query,date_query):
+    logger = logging.getLogger(__name__)
     query = {'district_id':f"{dist_query}",
              'date':f"{date_query}"}
     req = requests.get(call_url,headers=HEADERS,params=query)
 
     if req.status_code != 200:
         print(f"ERROR! {req.url} returned {req.status_code}")
+        if req.status_code == 403:
+            logger.error(f"403 FORBIDDEN")
+            sys.exit(1)
         return None
     else:
         return req.json()
+
+def get_day_for_district(dist_query,date_query):
+    call_url = urllib.parse.urljoin(ROOT_URL,DISTRICT_ENDPOINT)
+    res = get_for_district(call_url,dist_query, date_query)
+    if res and res['sessions']:
+        sites = parse_sessions(res['sessions'])
+        if sites:
+            print(f"{date_query:-^80}")
+            info = list(map(lambda x:{'name':x['name'],'pincode':x['pincode'],'vaccine':x['vaccine'],'available':x['available_capacity']},sites))
+            for center in info:
+                print(f"{center['name']}: {center['pincode']} : {center['vaccine']}: {center['available']}")
+
+   
+
+def get_week_for_district(dist_query, date_query):
+    call_url = urllib.parse.urljoin(ROOT_URL,DISTRICT_CALENDAR_ENDPOINT)
+    res = get_for_district(call_url,dist_query,date_query)
+    info = dict()
+    if res:
+        for center in res['centers']:
+            if center['sessions']:
+                sessions_by_date = {session['date']:{'name':center['name'],'available':session['available_capacity'],'pincode':center['pincode'],
+                    'vaccine': session['vaccine'],'fee':center['fee_type'],'address':center['address']}
+                                                     for session in center['sessions'] if session['min_age_limit'] == 18 and session['available_capacity']>0}
+            for date in sorted(sessions_by_date.keys()):
+                session = info.setdefault(date,[])
+                session.append(sessions_by_date[date])
+        return info
+    else:
+        return None
 
 def parse_sessions(raw_sessions):
     filtered_sessions = filter(lambda x: str(x["min_age_limit"]) == '18',raw_sessions)
@@ -91,19 +133,27 @@ def main():
     logger.setLevel(logging.INFO)
     sessions = list()
     state_to_get = 'Delhi'
+    #state_to_get = 'West Bengal'
     state_id = get_state_id(state_to_get)
+    #districts = list(filter(lambda x: x[0] in (144,149),get_districts(state_id)))
     districts = get_districts(state_id)
-    for dist_query in districts:
-        print(f"{dist_query:-^80}")
-        for date in range(1,32):
-            date_query = create_date_for_query(date,5,2021)
-            res = get_for_district(dist_query,date_query)
-            if res and res['sessions']:
-                sites = parse_sessions(res['sessions'])
-                if sites:
-                    print(f"{date_query:-^80}")
-                    info = map(lambda x:{'name':x['name'],'pincode':x['pincode']},sites)
-                    print(info)
+    
+
+    TODAY = datetime.date.today()
+    MAX_DAYS = monthrange(TODAY.year, TODAY.month)[1]
+
+    for date in range(TODAY.day,MAX_DAYS+TODAY.day,7):
+        date_query = create_date_for_query(date,TODAY.month,TODAY.year)
+        sessions_this_week = dict()
+        for dist_query,dist_name in districts:
+            sessions_this_week[dist_name] = get_week_for_district(dist_query,date_query)
+        for dist_name in sorted(sessions_this_week.keys()):
+            print(f"{dist_name:-^80}")
+            for date,sessions in sessions_this_week[dist_name].items():
+                print(f"{date:-^80}")
+                for session in sessions:
+                    print(f"{session['name']} - {session['address']} - {session['pincode']} - {session['vaccine']} - {session['fee']} - {session['available']}")
+
 
 if __name__ == '__main__':
     main()
